db9477ab4e45b574aa21fb8fe1fa0a90e4b85b09
Haseeb Shah <haseebs@outlook.com>
2017-01-13 23:47:52 +0500

    Added signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(permitted_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      render 'new';
+    end
+  end
+
+  def permitted_params
+    params.require(:user).permit(:battletag, :email, :password, :password_confirmation)
   end
 end
