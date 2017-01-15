ada426a2cbbd03135e25d22b3f7780fbaa3bf221
Haseeb Shah <haseebs@outlook.com>
2017-01-15 12:18:20 +0500

    Added sample data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,19 @@
 
 
 user = User.create(email: 'test@test.com', battletag: 'test#123', password: 'test123', password_confirmation: 'test123')
+Match.create(map: 1, rank: 2500, group_size: 2, user_id: 1, notes: 'Got trolled', video_link: 'www.youtube.com/v/sdfsfe3s')
+Match.create(map: 1, rank: 2500, group_size: 2, user_id: 1, notes: 'Got trolled', video_link: 'www.youtube.com/v/sdfsfe3s')
+Match.create(map: 1, rank: 2550, group_size: 1, user_id: 1, video_link: 'www.youtube.com/v/sdfsfe3s')
+Match.create(map: 1, rank: 2560, group_size: 2, user_id: 1)
+Match.create(map: 1, rank: 2540, group_size: 3, user_id: 1, notes: 'Got trolled', video_link: 'www.youtube.com/v/sdfsfe3s')
+Match.create(map: 1, rank: 2570, group_size: 2, user_id: 1, notes: 'Got trolled')
+
+Hero.create(match_id: 1, hero: 1)
+Hero.create(match_id: 1, hero: 3)
+Hero.create(match_id: 2, hero: 10)
+Hero.create(match_id: 3, hero: 1)
+Hero.create(match_id: 4, hero: 7)
+Hero.create(match_id: 4, hero: 2)
+Hero.create(match_id: 4, hero: 6)
+Hero.create(match_id: 5, hero: 4)
+Hero.create(match_id: 6, hero: 5)
