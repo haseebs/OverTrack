4d8b1b90f604f4f803a56bbe4af792443f0c8d21
Haseeb Shah <haseebs@outlook.com>
2017-01-15 13:21:51 +0500

    Added constants for maps, heroes and wld

--- app/helpers/constants_helper.rb
+++ app/helpers/constants_helper.rb
@@ -31,4 +31,64 @@ module ConstantsHelper
       'Watchpoint: Gibraltar'
     end
   end
+
+  def get_hero_name(hero_id)
+    case hero_id
+    when 1
+      'Ana'
+    when 2
+      'Bastion'
+    when 3
+      'D.Va'
+    when 4
+      'Genji'
+    when 5
+      'Hanzo'
+    when 6
+      'Junkrat'
+    when 7
+      'Lucio'
+    when 8
+      'Mcree'
+    when 9
+      'Mei'
+    when 10
+      'Mercy'
+    when 11
+      'Pharah'
+    when 12
+      'Reaper'
+    when 13
+      'Reinhardt'
+    when 14
+      'Roadhog'
+    when 15
+      'Soldier: 76'
+    when 16
+      'Sombra'
+    when 17
+      'Symmetra'
+    when 18
+      'Torbjorn'
+    when 19
+      'Widowmaker'
+    when 20
+      'Winston'
+    when 21
+      'Zarya'
+    when 22
+      'Zenyatta'
+    end
+  end
+
+  def get_wld(wld_val)
+    case wld_val
+    when 1
+      'Win'
+    when 2
+      'Lose'
+    when 3
+      'Draw'
+    end
+  end
 end
