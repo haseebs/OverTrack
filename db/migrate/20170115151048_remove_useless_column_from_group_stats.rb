be324ee921b2bc4b4e4ccd5f1c3bf4f4a5a67117
Haseeb Shah <haseebs@outlook.com>
2017-01-15 22:14:17 +0500

    Completed group_stats backend

---
+++ db/migrate/20170115151048_remove_useless_column_from_group_stats.rb
@@ -0,0 +1,7 @@
+class RemoveUselessColumnFromGroupStats < ActiveRecord::Migration[5.0]
+  def change
+    #remove_column :group_stats, :group_size
+    #remove_column :group_stats, :integer
+    #add_column    :group_stats, :group_size, :integer
+  end
+end
