34d050c83ee645e95d7ad38429337d5fcb19bec5
Haseeb Shah <haseebs@outlook.com>
2017-01-17 04:10:59 +0500

    Fixed charts showing wrong values

--- app/helpers/map_stats_helper.rb
+++ app/helpers/map_stats_helper.rb
@@ -3,7 +3,7 @@ module MapStatsHelper
     chart_data = Array.new(14, 0)
     stats = map_stats.pluck(:map, :total_games)
     stats.each do |stat|
-      chart_data[stat[0]] = stat[1]
+      chart_data[stat[0] - 1] = stat[1]
     end
     return chart_data
   end
