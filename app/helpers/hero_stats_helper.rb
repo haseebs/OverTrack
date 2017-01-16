0b65b6c972006264314f5436d8cdcce81f668707
Haseeb Shah <haseebs@outlook.com>
2017-01-17 01:32:01 +0500

    Fixed pie chart for hero stats

--- app/helpers/hero_stats_helper.rb
+++ app/helpers/hero_stats_helper.rb
@@ -1,3 +1,10 @@
 module HeroStatsHelper
-  @hero_names = ['Ana', 'Bastion','D.Va','Genji','Hanzo','Junkrat','Lucio','Mcree','Mei','Mercy','Pharah','Reaper', 'Reinhardt','Roadhog','Soldier: 76','Sombra','Symmetra','Torbjorn','Tracer','Widowmaker','Winston','Zarya','Zenyatta']
+  def get_chart_data(hero_stats)
+    chart_data = Array.new(23, 0)
+    stats = hero_stats.pluck(:hero, :total_games)
+    stats.each do |stat|
+      chart_data[stat[0]] = stat[1]
+    end
+    return chart_data
+  end
 end
