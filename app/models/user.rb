1bac1d02b556de320d29461958099bf751ced7cb
Haseeb Shah <haseebs@outlook.com>
2017-01-17 02:37:06 +0500

    Added form errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_secure_password
 end
